4e5ae96bfd83e0916d34a0a7810513150475d64d
dinh van binh <dinhvanbinh2310@gmail.com>
2024-11-22 23:14:31 +0700

    Initial commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private QLBanXeGanMayEntities db = new QLBanXeGanMayEntities(); // DbContext được tạo tự động từ Entity Framework
+
+        public ActionResult Index()
+        {
+            var danhSachXe = db.XEGANMAYs.ToList(); // Corrected property name
+            if (danhSachXe == null || !danhSachXe.Any())
+            {
+                return Content("Không có xe nào!");
+            }
+            return View(danhSachXe);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/textController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class textController : Controller
+    {
+        private QLBanXeGanMayEntities db = new QLBanXeGanMayEntities(); // DbContext được tạo tự động từ Entity Framework
+
+        public ActionResult Index()
+        {
+            // Truy vấn danh sách tên xe
+            var tenXeList = db.XEGANMAYs.Select(x => x.TenXe).ToList();
+
+            // Truyền danh sách tên xe sang View
+            ViewBag.TenXeList = tenXeList;
+
+            return View();
+        }
+    }
+}
